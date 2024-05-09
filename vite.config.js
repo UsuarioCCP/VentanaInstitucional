b378ec01e8b31b70867582b51e11993da2883d0a
archivoccp@gmail.com <archivoccp@gmail.com>
2024-05-09 16:15:59 -0500

    configurando nuevamente

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,21 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist", // Directorio de salida para los archivos generados
     rollupOptions: {
-      external: ["react", "react-dom", "react-router-dom"], // Indica las dependencias externas
+      external: ['react', 'react-dom', 'react-router-dom'], // Especifica las dependencias externas
       output: {
-        // Configura el formato de salida y las variables globales
+        // Configura el formato de salida
         globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-          "react-router-dom": "ReactRouterDOM",
+          react: 'React',               // Asocia 'react' con 'React' globalmente
+          'react-dom': 'ReactDOM',      // Asocia 'react-dom' con 'ReactDOM' globalmente
+          'react-router-dom': 'ReactRouterDOM', // Asocia 'react-router-dom' con 'ReactRouterDOM' globalmente
         },
+        // Puede ser necesario ajustar otros parámetros de salida según tus necesidades
+        // Por ejemplo, puedes especificar el formato deseado (iife, esm, cjs, etc.)
+        format: 'esm',  // Establece el formato de salida como ECMAScript Modules (ESM)
       },
     },
   },
