d5a56f40c8a1542f26efc0fb07dc1966f21562e1
archivoccp@gmail.com <archivoccp@gmail.com>
2024-05-09 14:53:27 -0500

    se que esta mal hacer esto pero estoy experimentando sobre la rama principal y cambiando e instalando algunos elementos

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,32 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: 'dist', // Directorio de salida para los archivos generados
+//     rollupOptions: {
+//       external: ['react', 'react-dom', 'react-router-dom'], // Indica las dependencias externas
+//       output: {
+//         // Configura el formato de salida y las variables globales
+//         globals: {
+//           react: 'React',
+//           'react-dom': 'ReactDOM',
+//           'react-router-dom': 'ReactRouterDOM',
+//         },
+//       },
+//     },
+//   },
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Directorio de salida para los archivos generados
-    rollupOptions: {
-      external: ['react', 'react-dom', 'react-router-dom'], // Indica las dependencias externas
-      output: {
-        // Configura el formato de salida y las variables globales
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-          'react-router-dom': 'ReactRouterDOM',
-        },
-      },
+  resolve: {
+    alias: {
+      // Elimina los alias para 'react' y 'react-dom'
     },
   },
 });
