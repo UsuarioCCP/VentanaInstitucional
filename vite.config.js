8bf9fb37a3bf212dd4e0509d31d4b782268d6532
archivoccp@gmail.com <archivoccp@gmail.com>
2024-05-09 14:32:23 -0500

    nueva modificacion al vite.config para intentar desplegar en netlify

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'dist', // Directorio de salida para los archivos generados
     rollupOptions: {
       external: ['react', 'react-dom', 'react-router-dom'], // Indica las dependencias externas
       output: {
-        // Configura el formato de salida
+        // Configura el formato de salida y las variables globales
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM',
